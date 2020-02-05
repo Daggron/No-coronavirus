057b63afd4254c3251908a9f2ca6631ae4ccf20e
Daggron <returnofking04@gmail.com>
2020-02-06 01:27:41 +0530

    Complete extension

---
+++ script.js
@@ -0,0 +1,9 @@
+replaceText(document.body)
+
+function replaceText(element){
+    if(element.hasChildNodes()){
+        element.childNodes.forEach(replaceText)
+    }else if(element.nodeType === Text.TEXT_NODE){
+        element.textContent = element.textContent.replace(/coronavirus/gi,'░░█▒ █▒▒ █▓▓░');
+    }
+}
